c72077db913d20cc27f3153f7ae4cc5bfd40f78f
leequiro <leandroemanuel.quiroga@nttdata.com>
2023-01-21 14:30:28 -0300

    env: New environment

--- configuration/environment.ts
+++ configuration/environment.ts
@@ -4,4 +4,6 @@ export const environment: Environment = {
   PORT: process.env.PORT || '',
   HOST: process.env.HOST || '',
   URI_DB: process.env.URI_DB || '',
+  STDTLL: Number(process.env.STDTLL) || 10,
+  PATH_FILE: process.env.PATH_FILE || '',
 }
