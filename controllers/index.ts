18e00bb5cfe6a44d5ef7b8c70b43e96a7c9e4d20
leequiro <leandroemanuel.quiroga@nttdata.com>
2023-01-20 23:39:54 -0300

    controller: Has been added new controller

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,6 +1,33 @@
 import { Request, Response } from 'express';
+import zoneSchema from '../schemas/zone.schema';
+import { readFileXlsx, responeBadRequest } from '../utilities/index';
+import path from 'path';
+import { Zone } from '../interfaces/index';
 
 
-export const getController = (req: Request, res: Response) => {
-  res.send('Hola Mundo')
+
+export const getFileDB = (req: Request, res: Response) => {
+  try {
+    const fileData = zoneSchema.find();
+    if (!fileData)
+      return res.status(400).json(responeBadRequest);
+
+    res.status(200).json({
+      fileData
+    });
+  } catch (err: any) {
+    throw new Error(err)
+  }
 };
+
+export const postUpLoadFileDB = async (req: Request, res: Response) => {
+  const pathFile = path.join(__dirname, '../uploads/Localidades Competitivas.xlsx');
+
+  try {
+    const arrData: any[] = readFileXlsx(pathFile)
+    res.status(200).json(arrData)
+  } catch (err: any) {
+    console.log(err);
+    throw new Error(err);
+  };
+}
