4700b0e0758f34649ce12875aae463671d51c80e
leequiro <leandroemanuel.quiroga@nttdata.com>
2023-01-20 23:45:53 -0300

    server: Has been updated and added new method errorHandle and middleware

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,11 @@
 import express, { Express } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
 import { environment } from '../configuration/environment';
+import { dbConnect } from '../database/db.connect';
 import { router } from '../router';
+import { handleNotFound, errorServer } from '../middlewares/index';
 
 class ServerApp {
   app: Express;
@@ -12,13 +17,31 @@ class ServerApp {
     this.port = environment.PORT;
     this.host = environment.HOST;
 
+    this.middleware();
+    this.connectDataBase();
+    this.router();
+    this.errorHandle();
+  };
 
-    this.router()
+  middleware(): void {
+    this.app.use(cors());
+    this.app.use(bodyParser.urlencoded({
+      extended: true
+    }));
+    this.app.use(bodyParser.json());
+  };
+
+  async connectDataBase(): Promise<void> {
+    await dbConnect();
   }
   router(): void {
     this.app.use('/v1', router)
   };
 
+  errorHandle(): void {
+    this.app.use(handleNotFound);
+    this.app.use(errorServer)
+  };
 
   listen(): void {
     this.app.listen(this.port, () => {
