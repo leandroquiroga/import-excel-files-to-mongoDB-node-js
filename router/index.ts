ae48fc4005e4298a5a67d75cf54549111acab7be
leequiro <leandroemanuel.quiroga@nttdata.com>
2023-01-21 14:33:03 -0300

    router: Has been added news routes

--- router/index.ts
+++ router/index.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { postUpLoadFileDB, getFileDB } from '../controllers/index';
+import { postUpLoadFileDB, getFileDB, getFileByIdDB, getFileByZoneDB } from '../controllers/index';
 
 const router: Router = Router();
 
 router.get('/data', getFileDB);
+
+router.get('/data/:code', getFileByIdDB);
+
+router.get('/data/:zone/:province', getFileByZoneDB);
+
 router.post('/upload', postUpLoadFileDB);
 
 export {
