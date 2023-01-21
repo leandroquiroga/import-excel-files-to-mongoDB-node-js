2d73d06c36ebac9d352529c96f91f17d6e927f28
leequiro <leandroemanuel.quiroga@nttdata.com>
2023-01-20 23:40:29 -0300

    interface: Has been created a new interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,5 +1,23 @@
+export interface HandleError {
+  message: string;
+  code: number;
+}
 export interface Environment {
   PORT: string;
   HOST: string;
   URI_DB: string;
 };
+
+export interface Zone {
+  code: number;
+  central: string;
+  zone: string;
+  locality: string;
+  province: string
+}
+
+export interface BadRequestResponse {
+  code: number;
+  status: boolean;
+  message: string;
+}
