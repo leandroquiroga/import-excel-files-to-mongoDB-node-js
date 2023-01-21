b4cd9c86467b053b7f5f2b11b402d56e6b2fa981
leequiro <leandroemanuel.quiroga@nttdata.com>
2023-01-21 14:31:39 -0300

    controller: Has been added a news controller - getFileByIdDB, getFileByZoneDB

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,33 +1,80 @@
-import { Request, Response } from 'express';
-import zoneSchema from '../schemas/zone.schema';
-import { readFileXlsx, responeBadRequest } from '../utilities/index';
 import path from 'path';
-import { Zone } from '../interfaces/index';
+import { Request, Response } from 'express';
+import NodeCache from 'node-cache';
 
+import zoneSchema from '../schemas/zone.schema';
+import { environment } from '../configuration/environment';
+import { invalidRequest, readFileXlsx, responseBadRequest, responseErrorServer } from '../utilities/index';
+import { ResponseOk, RequestResponse } from '../interfaces/index';
 
+const cache = new NodeCache({ stdTTL: environment.STDTLL });
 
-export const getFileDB = (req: Request, res: Response) => {
+export const getFileDB = async (req: Request, res: Response)
+  : Promise<Response<ResponseOk[], Record<number, RequestResponse>> | undefined> => {
   try {
-    const fileData = zoneSchema.find();
-    if (!fileData)
-      return res.status(400).json(responeBadRequest);
+    // Chequea si los datos entan en cache
+    if (cache.has('zone')) {
+      const response = cache.get('zone')
+      res.status(200).json(response);      
+      return
+    };
+    const fileData: ResponseOk[] | null = await zoneSchema.find();
+    if (!fileData) return res.status(400).json(responseBadRequest);
 
-    res.status(200).json({
-      fileData
-    });
+    // Seteamos cache con los datos de la base de datosgetFileByZoneDB
+    cache.set('zone', fileData);
+    res.status(200).json(fileData);      
   } catch (err: any) {
-    throw new Error(err)
+    res.status(500).json(responseErrorServer);
   }
 };
 
-export const postUpLoadFileDB = async (req: Request, res: Response) => {
-  const pathFile = path.join(__dirname, '../uploads/Localidades Competitivas.xlsx');
-
+export const getFileByIdDB = async (req: Request, res: Response)
+  : Promise<Response<ResponseOk, Record<number, RequestResponse>>> => {
   try {
-    const arrData: any[] = readFileXlsx(pathFile)
-    res.status(200).json(arrData)
+    const { code } = req.params;
+    const zoneByCode: ResponseOk | null = await zoneSchema.findOne({ code });
+
+    if (!zoneByCode) return res.status(400).json(responseBadRequest);
+    return res.status(200).json(zoneByCode);
   } catch (err: any) {
-    console.log(err);
+    res.status(500).json(responseErrorServer);
     throw new Error(err);
+  }
+};
+
+// Solo devuelve con dos patametros (Revisar)
+export const getFileByZoneDB = async (req: Request, res: Response) => {
+  try {
+    const { zone, province } = req.params;
+    
+    if ([zone, province].includes('')) return res.status(400).json(invalidRequest);
+
+    // Retorna el codigo y la central segun la coincidencia que hay con los valores pasados por parametros
+    const zones: ResponseOk[] | ResponseOk | null = await zoneSchema.find(
+                                                                { zone, province },
+                                                                { code: 1, _id: 0, central: 1 }
+                                                              ); 
+    
+    if (!zones) return res.status(400).json(responseBadRequest);
+    return res.status(200).json(zones); 
+  } catch (err: any) {
+    res.status(500).json(responseErrorServer);
+    throw new Error(err);
+  }
+};
+
+export const postUpLoadFileDB = (req: Request, res: Response): void => {
+  // Nos quedamos con la ruta en donde se encuentra el archivo
+  const pathFile = path.join(__dirname, `${environment.PATH_FILE}`);
+  try {
+    const arrData: any[] = readFileXlsx(pathFile);
+    arrData.forEach(async data => {
+      const zone = new zoneSchema(data);
+      await zone.save();
+    })
+    res.status(200).json(arrData);
+  } catch (err: any) {
+    res.status(500).json(responseErrorServer);
   };
 }
